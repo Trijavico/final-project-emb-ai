177b6d6002bed8af0767d4aefec907fd6152af5e
Javier Peña Perez <javier.penaperez@gmail.com>
2024-07-21 16:21:59 -0400

    Package emotion detection module and format the result by json encode

---
+++ EmotionDetection/emotion_detection.py
@@ -0,0 +1,23 @@
+import requests, json
+
+def emotion_detector(text_to_analyze):
+    URL = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    Headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    input = { "raw_document": { "text": text_to_analyze } }
+
+    response = requests.post(url= URL, json= input, headers= Headers)
+
+    response_dict = json.loads(response.text)
+    emotions = response_dict.get('emotion', {})
+    emotion_scores = {
+        'anger': emotions.get('anger', 0),
+        'disgust': emotions.get('disguts', 0),
+        'fear': emotions.get('fear', 0),
+        'joy': emotions.get('joy', 0),
+        'sadness': emotions.get('sadness', 0)
+    }
+
+    dominant = max(emotion_scores, key=emotion_scores.get)
+    emotion_scores['dominant_emotion'] = dominant
+
+    return emotion_scores
