582584743964b3eb1bbc65c6faad1fffbfe7747d
Javier Peña Perez <javier.penaperez@gmail.com>
2024-07-21 15:49:46 -0400

    Add emotion detection module

---
+++ emotion_detection.py
@@ -0,0 +1,10 @@
+import requests
+
+def emotion_detector(text_to_analyze):
+    URL = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    Headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    input = { "raw_document": { "text": text_to_analyse } }
+
+    response = requests.post(url= URL, json= input, headers= Headers)
+
+    return response.text
